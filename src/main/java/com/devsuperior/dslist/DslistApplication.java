de9679896e5c0f2a43230128dfe59d4ca99e2c3c
david <you@example.com>
2023-05-08 23:55:32 -0300

    properties,games

--- src/main/java/com/devsuperior/dslist/DslistApplication.java
+++ src/main/java/com/devsuperior/dslist/DslistApplication.java
@@ -1,13 +1,13 @@
-package com.devsuperior.sdlist;
+package com.devsuperior.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SdlistApplication {
+public class DslistApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SdlistApplication.class, args);
+		SpringApplication.run(DslistApplication.class, args);
 	}
 
 }
--- src/test/java/com/devsuperior/dslist/DslistApplicationTests.java
+++ src/test/java/com/devsuperior/dslist/DslistApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devsuperior.sdlist;
+package com.devsuperior.dslist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SdlistApplicationTests {
+class DslistApplicationTests {
 
 	@Test
 	void contextLoads() {
