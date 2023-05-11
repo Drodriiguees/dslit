f61f7bc519fc312ead77a30d182f7660b07e687a
david <you@example.com>
2023-05-11 11:36:27 -0300

    Create List

---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,67 @@
+package com.devsuperior.dslist.entities;
+
+import org.hibernate.mapping.List;
+
+import java.util.Objects;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private Belongingpk id = new Belongingpk();
+
+    private Integer position; 
+
+    public Belonging(){
+
+    }
+
+    public Belonging(Game game, GameList list, Integer position) {
+        id.setGame(game);
+        id.setList(list);
+        this.position = position;
+    }
+
+    public Belongingpk getId() {
+        return id;
+    }
+
+    public void setId(Belongingpk id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Belonging other = (Belonging) obj;
+            return  Objects.equals(id, other.id);
+        
+    }
+
+}
